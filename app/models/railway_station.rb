add292bc17e25b7e9906900d2a9f1a031b31a647
Botanik1592 <anton.buryka@gmail.com>
2016-09-02 10:09:27 +0300

    Валидация перенесена под ассоциации. В ассоциации с билетами добавлен тип has_many с форейн_кеями и разными названиями

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,8 +1,9 @@
 class RailwayStation < ApplicationRecord
-  validates :title, presence: true
-
-  has_many :trains
-  has_many :tickets
+  has_many :train
+  has_many :departure_tickets, class_name: 'Ticket', foreign_key: :first_station_id
+  has_many :destination_tickets, class_name: 'Ticket', foreign_key: :last_station_id
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
+
+  validates :title, presence: true
 end
