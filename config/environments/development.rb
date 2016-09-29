88b39fcf35d8a833f9236b6fefae26a6976d739f
Botanik1592 <anton.buryka@gmail.com>
2016-09-29 13:22:19 +0300

    Russian language for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
+
+  config.i18n.default_locale = :ru
+  config.i18n.locale = :ru
 end
