92867c8fc4fb201098c46c58b3ab63e0a29e50c7
Botanik1592 <anton.buryka@gmail.com>
2016-10-01 21:37:28 +0300

    Some fixes (validates and messages)

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,6 +5,8 @@ class Ticket < ApplicationRecord
   belongs_to :last_station, class_name: 'RailwayStation', foreign_key: :last_station_id
 
   validates :user_name, :passport, presence: true
+  validates :passport, format: { with: /\A[[:digit:]]{4}[\s]+[[:digit:]]{6}\z/ }
+  validates :user_name, length: { minimum: 3, maximum: 50 }
 
   after_create :send_notification
   before_destroy :save_ticket_params
